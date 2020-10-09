fda1e92183a95a0d3046c660b8bbd62e8dabe0c4
HannahDeWachter <r0738008@student.thomasmore.be>
2020-10-09 16:09:44 +0200

    job seed

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Job;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JobFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Job::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->jobTitle,
+            'description' => $this->faker->realText(200),
+            'start_date' => $this->faker->date($format = 'Y-m-d', $min = 'now'),
+            'company_id' => $this->faker->numberBetween($min = 1, $max = 5)
+        ];
+    }
+}
--- database/seeders/JobsSeeder.php
+++ database/seeders/JobsSeeder.php
@@ -4,15 +4,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class JobsSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(JobsSeeder::class);
+        //
+        \App\Models\Job::factory(5)->create();
     }
 }
