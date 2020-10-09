17fde192fc91e155fe88acdb88e582b989eee19a
HannahDeWachter <r0738008@student.thomasmore.be>
2020-10-09 15:55:45 +0200

    company seed

---
+++ database/seeders/CompaniesSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CompaniesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $company = new \App\Models\Company();
+        $company->name = "Microsoft";
+        $company->location = "Washington";
+        $company->description = "Computer bedrijf";
+        $company->email = "microsoft@outlook.com";
+        $company->rating = 0;
+        $company->save();
+        $company = new \App\Models\Company();
+        $company->name = "Apple";
+        $company->location = "California";
+        $company->description = "Bedrijf van iPhone";
+        $company->email = "apple@imail.com";
+        $company->rating = 0;
+        $company->save();
+        $company = new \App\Models\Company();
+        $company->name = "Alternate";
+        $company->location = "Antwerpen";
+        $company->description = "Verkoper van computers";
+        $company->email = "alternate@hotmail.com";
+        $company->rating = 0;
+        $company->save();
+        $company = new \App\Models\Company();
+        $company->name = "Intracto";
+        $company->location = "Tilburg";
+        $company->description = "Digital agency";
+        $company->email = "intracto@gmail.com";
+        $company->rating = 0;
+        $company->save();
+    }
+}
