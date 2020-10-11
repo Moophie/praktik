e293bee9ede7ec5bdda66262294957e424ede9d9
Madoska <medina-dadurgova@email.com>
2020-10-11 20:07:16 +0200

    created internship page+ added filler content for all pages

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student settings</title>
+</head>
+<body>
+    <h1>Profile settings</h1>
+    This is where you can make changes to your profile!
+    <h3>Portfolio</h3>
+    <p>Add more items to your portfolio</p>
+</body>
+</html>
--- resources/views/vacancy.blade.php
+++ resources/views/vacancy.blade.php
@@ -3,21 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student</title>
+    <title>Stage</title>
 </head>
 <body>
-    <h1>Plain Jane</h1>
-    <p>This is a student description!</p>
-    <h3>Portfolio</h3>
+    <h1>Stagiar developer gezocht</h1>
+    <article>Looking for an enthousiastic Javascript developer! Yada yada</article>
     <img src="https://via.placeholder.com/150" alt="">
     <img src="https://via.placeholder.com/150" alt="">
     <img src="https://via.placeholder.com/150" alt="">
-    <h3>CV</h3>
-    <li>this</li>
-    <li>is</li>
-    <li>where</li>
-    <li>my</li>
-    <li>cv</li>
-    <li>goes</li>
 </body>
 </html>
