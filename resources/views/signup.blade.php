1db3c7143e6d933a05c97a4a44354bb0c81f8069
Madoska <medina-dadurgova@email.com>
2020-10-11 19:01:40 +0200

    created basic views

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Signup</h1>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="loginForm">
+            <form action="" method="get">
+                <label for="firstname">First name</label>
+                <input type="text" name="firstname" id="firstname">
+
+                <label for="lastname">Last name</label>
+                <input type="text" name="lastname" id="lastname">
+
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email">
+
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+
+                <label for="confirmPassword">Confirm password</label>
+                <input type="password" name="confirmPassword" id="password">
+
+                <input type="submit" value="Signup">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
