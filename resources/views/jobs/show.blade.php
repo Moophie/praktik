c162a292dfcf1817cfcc4f814fc46c1253a85458
Medina <medina-dadurgova@hotmail.com>
2020-10-25 19:09:54 +0100

    added simple detail page for jobs

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,1 +1,6 @@
-test
+<h1>{{ $job->name }}</h1>
+<h3><a href="#">Company {{ $job->company_id }}</a></h3>
+<p>{{ $job->description }}</p>
+<p>{{ $job->start_date }}</p>
+
+<a href="/applications/create">Apply now</a>
