6d5fb34ff1f104a4fe99a37ec8c3d46c3a01b225
HannahDeWachter <r0738008@student.thomasmore.be>
2020-10-30 11:30:36 +0100

    apply css to buttons

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -11,8 +11,13 @@ Student settings
 
 <div class="Wrapper bg-light">
     <h1>Profile settings</h1>
-    This is where you can make changes to your profile!
-    <form action="/upload" method="post" enctype="multipart/form-data">
+    @if(Auth::check())
+    <h2>Hello {{ Auth::user()->firstname }}!</h2>
+    @else
+    <div class="alert alert-info">Please log in</div>
+    @endif
+    <p>This is where you can make changes to your profile!</p>
+    <form class="form-group" action="/upload" method="post" enctype="multipart/form-data">
         {{ csrf_field() }}
 
         <label for="image">Profile picture</label><br>
@@ -22,11 +27,11 @@ Student settings
         <label for="cv">CV</label><br>
         <input type="file" name="cv">
         <br><br>
-        <input type="submit" value="Upload">
+        <input type="submit" class="btn btn-primary" value="Upload">
     </form>
     <br>
     <h3>Portfolio</h3>
-    <a href="/dribbble" class="button">Import shots from Dribbble</a>
+    <a href="/dribbble" class="btn btn-primary">Import shots from Dribbble</a>
 </div>
 
 @endsection
