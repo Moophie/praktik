b6b25def66fd8d457b094fc288488e6c34dabd5a
Moophie <michael.van.lierde@hotmail.com>
2020-10-30 11:54:58 +0100

    Company page layout

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -1,79 +1,102 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts/app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create a company</title>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+@section('title')
+    Companies
+@endsection
+
+@section('extrascripts')
     <script src=" {{ asset('js/ajax.js') }}"></script>
+@endsection
+
+@section('csrftoken')
     <meta name="csrf-token" content="{{ csrf_token() }}">
-</head>
+@endsection
 
-<body>
-    <h1>Your company</h1>
-    <form method="POST" action="/companies">
-        @csrf
+@section('content')
 
-        <div class="form-group">
-            <label for="name">user_id</label>
-            <input value="3" type="text" class="form-control" id="user_id" name="user_id">
-        </div>
+    @component('components/navbar')
 
-        <div class="form-group">
-            <label for="name">geolat</label>
-            <input type="number" class="form-control" id="geolat" name="geolat" step="any">
-        </div>
+    @endcomponent
 
-        <div class="form-group">
-            <label for="name">geolng</label>
-            <input type="number" class="form-control" id="geolng" name="geolng" step="any">
-        </div>
 
-        <div class="form-group">
-            <label for="name">Company name</label>
-            <input type="text" class="form-control" id="name" name="name">
-        </div>
+    <div class="wrapper bg-light">
+        <form method="POST" action="/companies">
+            <h1>Your company <button type="submit" class="btn btn-primary float-right">Save Company</button></h1>
+            @csrf
 
-        <div class="form-group">
-            <label for="city">City</label>
-            <input type="text" class="form-control" id="city" name="city">
-        </div>
+            <div class="form-group row">
+                <input value="3" type="text" class="form-control" id="user_id" name="user_id" hidden>
+            </div>
 
-        <div class="form-group">
-            <label for="address">Address</label>
-            <input type="text" class="form-control" id="address" name="address">
-        </div>
+            <div class="form-group row">
+                <label for="name" class="col-sm-2 col-form-label">Latitude</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control" id="geolat" name="geolat" step="any">
+                </div>
+            </div>
 
-        <div class="form-group">
-            <label for="logo">Logo</label>
-            <input type="text" class="form-control" id="logo" name="logo">
-        </div>
+            <div class="form-group row">
+                <label for="name" class="col-sm-2 col-form-label">Longitude</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control" id="geolng" name="geolng" step="any">
+                </div>
+            </div>
 
-        <div class="form-group">
-            <label for="website">Website</label>
-            <input type="text" class="form-control" id="website" name="website">
-        </div>
+            <div class="form-group row">
+                <label for="name" class="col-sm-2 col-form-label">Company name</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="name" name="name">
+                </div>
+            </div>
 
-        <div class="form-group">
-            <label for="description">Description</label>
-            <textarea name="description" id="description" cols="30" rows="10"></textarea>
-        </div>
+            <div class="form-group row">
+                <label for="city" class="col-sm-2 col-form-label">City</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="city" name="city">
+                </div>
+            </div>
 
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" name="email">
-        </div>
+            <div class="form-group row">
+                <label for="address" class="col-sm-2 col-form-label">Address</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="address" name="address">
+                </div>
+            </div>
 
-        <div class="form-group">
-            <label for="phone">Phone</label>
-            <input type="tel" class="form-control" id="phone" name="phone">
-        </div>
+            <div class="form-group row">
+                <label for="logo" class="col-sm-2 col-form-label">Logo</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="logo" name="logo">
+                </div>
+            </div>
 
-        <button type="submit" class="btn btn-primary">Save Company</button>
-    </form>
-</body>
+            <div class="form-group row">
+                <label for="website" class="col-sm-2 col-form-label">Website</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="website" name="website">
+                </div>
+            </div>
 
-</html>
+            <div class="form-group row">
+                <label for="description" class="col-sm-2 col-form-label">Description</label>
+                <div class="col-sm-10">
+                    <textarea name="description" class="form-control" id="description" cols="30" rows="6"></textarea>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="email" name="email">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="phone" class="col-sm-2 col-form-label">Phone</label>
+                <div class="col-sm-10">
+                    <input type="tel" class="form-control" id="phone" name="phone">
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -10,14 +10,20 @@
 
     @endcomponent
 
-    <button class="btn btn-primary"><a href="/companies/create">Create your own</a></button>
+    <div class="wrapper bg-light">
+        <button class="btn btn-primary "><a href="/companies/create" style="color:white">Create your own</a></button>
+        <br>
+        <br>
 
-    <h1>All companies</h1>
+        <h1>All companies</h1>
 
-    @foreach ($companies as $company)
-        <h3><a href="/companies/{{ $company->id }}">{{ $company->name }}</a></h3>
-        <h5>{{ $company->city }}</h5>
-        <p>{{ $company->description }}</p>
-    @endforeach
+        @foreach ($companies as $company)
+            <div class="jumbotron">
+                <h3><a href="/companies/{{ $company->id }}">{{ $company->name }}</a></h3>
+                <h5>{{ $company->city }}</h5>
+                <p>{{ $company->description }}</p>
+            </div>
+        @endforeach
+    </div>
 
 @endsection
