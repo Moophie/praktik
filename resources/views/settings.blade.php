9cd6c80a4d41e3a966e9e8d12e1c6a5703467aba
HannahDeWachter <r0738008@student.thomasmore.be>
2020-10-30 11:19:08 +0100

    form in div

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -12,25 +12,21 @@ Student settings
 <div class="Wrapper bg-light">
     <h1>Profile settings</h1>
     This is where you can make changes to your profile!
+    <form action="/upload" method="post" enctype="multipart/form-data">
+        {{ csrf_field() }}
+
+        <label for="image">Profile picture</label><br>
+        <input type="file" name="image">
+        <br>
+        <br>
+        <label for="cv">CV</label><br>
+        <input type="file" name="cv">
+        <br><br>
+        <input type="submit" value="Upload">
+    </form>
+    <br>
     <h3>Portfolio</h3>
-    <p>Add more items to your portfolio</p>
+    <a href="/dribbble" class="button">Import shots from Dribbble</a>
 </div>
-
-
-<form action="/upload" method="post" enctype="multipart/form-data">
-    {{ csrf_field() }}
-
-    <label for="image">Profile picture</label><br>
-    <input type="file" name="image">
-    <br>
-    <br>
-    <label for="cv">CV</label><br>
-    <input type="file" name="cv">
-    <br><br>
-    <input type="submit" value="Upload">
-</form>
-<br>
-<h3>Portfolio</h3>
-<a href="/dribbble" class="button">Import shots from Dribbble</a>
 
 @endsection
