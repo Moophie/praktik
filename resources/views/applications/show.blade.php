e8e7f4dde354aa28f3d32d43bea4e017f26f5a54
Medina <medina-dadurgova@hotmail.com>
2020-12-28 20:26:02 +0100

    added modal for label change

--- resources/views/applications/show.blade.php
+++ resources/views/applications/show.blade.php
@@ -14,7 +14,7 @@
         <div class="top">
             <h1>Application for {{ $application->job->name }}</h1>
             <h3>by {{ $application->user->firstname }} {{ $application->user->lastname }}</h3>
-            <p>on {{ $application->updated_at }} — {{$application->label->name }} <a href="">change label</a></p>
+            <p>on {{ $application->updated_at }} — {{$application->label->name }} </p><button type="button" class="btn btn-primary label" onclick='openModal()'>change label</button>
         </div>
     </div>
 
@@ -48,17 +48,70 @@
 
         <div class="contact">
             <h5>Contact information</h4>
-            @if(isset($application->user->email))
-            <a href="mailto:{{ $application->user->email }}">{{ $application->user->email }}</a>
-            @else
-            <p>No email given.</p>
-            @endif
+                @if(isset($application->user->email))
+                <a href="mailto:{{ $application->user->email }}">{{ $application->user->email }}</a>
+                @else
+                <p>No email given.</p>
+                @endif
         </div>
     </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-modal="true" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Application label</h5>
+                    <button type="button" class="close" aria-label="Close" onclick="closeModal()">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="">
+                        <label for="label">Change label</label>
+                        <select name="label" id="label">
+                            <option value="new">new</option>
+                            <option value="starred">star</option>
+                            <option value="approved">approve</option>
+                            <option value="declined">decline</option>
+                        </select>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" onclick="closeModal()">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal-backdrop fade show" id="backdrop" style="display: none;"></div>
+
 
 
 
 </div>
+
+<script>
+    function openModal() {
+        document.getElementById("backdrop").style.display = "block"
+        document.getElementById("exampleModal").style.display = "block"
+        document.getElementById("exampleModal").className += "show"
+    }
+
+    function closeModal() {
+        document.getElementById("backdrop").style.display = "none"
+        document.getElementById("exampleModal").style.display = "none"
+        document.getElementById("exampleModal").className += document.getElementById("exampleModal").className.replace("show", "")
+    }
+    // Get the modal
+    var modal = document.getElementById('exampleModal');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            closeModal()
+        }
+    }
+</script>
 
 
 @endsection
