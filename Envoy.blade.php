20a1dde27c2ec20de8df6b923f3e8d100caaf57c
Moophie <michael.van.lierde@hotmail.com>
2021-01-06 00:50:57 +0100

    envoy now sends confirmation to Discord server

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -12,6 +12,7 @@ composer
 @else
     cd /home/deploybot/praktik-staging
 @endif
+
 git pull
 
 @endtask
@@ -20,9 +21,22 @@ git pull
 
 @if ($production == 'yes')
     cd /home/deploybot/praktik
+
+    @setup
+    $message = get_current_user() . " probably messed up production right now. :(";
+    @endsetup
 @else
     cd /home/deploybot/praktik-staging
+
+    @setup
+    $message = get_current_user() . " just updated the staging website so changes can be extensively tested first :)";
+    @endsetup
 @endif
+
 composer install
+
+@finished
+@discord('https://discord.com/api/webhooks/796157285579096075/qflJ9Y8Fr6ROiU3dX9DMRzcjziBt25-RcUqb2ognY09SFt9R1F7bWXqfbYldOkOw0qNn', $message)
+@endfinished
 
 @endtask
