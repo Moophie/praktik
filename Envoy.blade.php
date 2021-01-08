62ee8171f069e14da9a563f0370d92b10e645d35
Moophie <michael.van.lierde@hotmail.com>
2021-01-08 03:00:31 +0100

    Removed Envoy comments

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,8 +1,5 @@
-<!-- The server and user that hosts the website: authentication through api-key -->
 @servers(['praktik' => ['deploybot@139.162.166.91']])
 
-<!-- Deploy the project to the server -->
-<!-- Use the flag --production=yes to deploy to production -->
 @task('deploy')
 
 @if ($production == 'yes')
@@ -14,14 +11,12 @@
 php artisan down
 git reset --hard HEAD
 git pull origin main
-<!-- Composer install, not update: newer versions on the server might break the application -->
 composer install
 php artisan migrate --force
 php artisan up
 
 @endtask
 
-<!-- Pause the server websites -->
 @task('pause')
 
 @if ($production == 'yes')
@@ -34,7 +29,6 @@ php artisan down
 
 @endtask
 
-<!-- Resume the server websites -->
 @task('resume')
 
 @if ($production == 'yes')
@@ -49,7 +43,6 @@ php artisan up
 
 @after
 
-<!-- Formulate the appropriate message based on task and production vs. staging -->
 if($task == "pause"){
     if($production == 'yes'){
     $message = get_current_user() . " paused the production website.";
@@ -74,7 +67,6 @@ if($task == "deploy"){
     }
 }
 
-<!-- Send the message to the Discord server -->
 @discord('https://discord.com/api/webhooks/796157285579096075/qflJ9Y8Fr6ROiU3dX9DMRzcjziBt25-RcUqb2ognY09SFt9R1F7bWXqfbYldOkOw0qNn', $message)
 
 @endafter
