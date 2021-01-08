8aabd0301f341701abeb45f66c9e91b17d11c054
Moophie <michael.van.lierde@hotmail.com>
2021-01-08 13:02:12 +0100

    Missed a conflict

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,18 +27,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
     </script>
-<<<<<<< HEAD
-
-    @yield('extrascripts')
-
-=======
     
     <script src="{{asset('js/app.js')}}"></script>
     @yield('extrascripts')
     <script ></script>
     
     <script src=" asset('/js/app.js')"></script>
->>>>>>> Vue.JS
 </body>
 
 </html>
