d960900ac5555ade48bd714948af8aac1f1395ac
Medina <medina-dadurgova@hotmail.com>
2021-01-08 21:46:34 +0100

    fixed link to application detils

--- app/Http/Controllers/ApplicationsController.php
+++ app/Http/Controllers/ApplicationsController.php
@@ -16,9 +16,10 @@ class ApplicationsController extends Controller
         if ($user->type === "student") {
             $data['applications'] = Application::with('user', 'job', 'label')->where('applications.user_id', '=', $user->id)->get();
         } else { // If the user is not a student, get all the applications made to the user's company
-            $data['applications'] = Application::join('jobs', 'jobs.id', '=', 'applications.job_id')->join('users', 'users.id', '=', 'applications.user_id')->join('companies', 'jobs.company_id', '=', 'companies.id')->select('jobs.*', 'applications.*', 'users.*')->where('companies.user_id', '=', $user->id)->get();
+            $data['applications'] = Application::join('jobs', 'jobs.id', '=', 'applications.job_id')->join('users', 'users.id', '=', 'applications.user_id')->join('companies', 'jobs.company_id', '=', 'companies.id')->select('jobs.*', 'applications.*', 'users.id as user_id', 'users.*', 'applications.id as app_id')->where('companies.user_id', '=', $user->id)->get();
         }
 
+        
         return view('applications/index', $data);
     }
 
